4aee7d53a0cf77e33cb3e9a3c3d33ee033c9afd9
sachinaneboinwad87-ship-it <sachinaneboinwad87@gmail.com>
2025-10-03 16:05:55 +0530

    DAy 1 work

---
+++ day_1_basic.py
@@ -0,0 +1,28 @@
+# Day 1 start from basics of python
+# Ways to declare a variable name.
+MyName = "Sachin" # pascal Case
+myName = "Sachin" # Camel case
+myname = "sachin" # Flat case
+my_name = "Sachin" # Snake case
+
+# we can aslsign multiple variables with multiple value
+a,b,c =1,2,3
+print(a,b,c)
+
+
+# Rules to declare variable name
+# 1. variable name must start with : letter or _ .
+_My_Name = " sachin"
+My_Name = "Sachin"
+
+# 2. variable name contain only 3 things : Letter , Numbers & Underscore (_)
+_1 = 10
+_term1 = "python"
+
+# 3. Variables are case sensitive
+Name ="sachin"
+#print(name) # Name & name are different
+print(Name)
+
+# 4. Variables name can not be reserved word
+#for = 10 # For  is a reserved word
